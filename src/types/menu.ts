ed4aab5404d26d079f665ac1b26cd251c972c8b8
neitan-02 <hernandezmorenonathan09@gmail.com>
2025-11-01 18:08:14 -0600

    types menu.ts se agrego interface EducationalContent para sección de menu educativo

---
+++ src/types/menu.ts
@@ -0,0 +1,9 @@
+// Agregar estas interfaces al archivo types existente
+export interface EducationalContent {
+  title: string;
+  description: string;
+  icon: string;
+  color: [string, string, ...string[]];
+  audioType: string;
+}
+
