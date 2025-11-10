af2bf55203def8e1d4aaafda05bf246d9c481c3d
neitan-02 <hernandezmorenonathan09@gmail.com>
2025-11-10 01:35:12 -0600

    test:se agrego health-check.test para verificar la funcionalidad de jest

---
+++ src/auth/__tests__/health-check.test.ts
@@ -0,0 +1,8 @@
+test('Jest está funcionando', () => {
+  console.log('Prueba de jest que esta en funcion');
+  expect(1 + 1).toBe(2);
+});
+
+test('Suma básica funciona', () => {
+  expect(2 + 2).toBe(4);
+});
